46723ee7509c7a5ca700079c1b1ef6d7d772fe86
Mateusz Urbańczyk <mateusz.urbanczyk97@gmail.com>
2016-07-03 03:40:00 +0200

    Token instance; starting with interpreter

---
+++ interpreter.py
@@ -0,0 +1,26 @@
+INTEGER, PLUS, EOF = 'INTEGER', 'PLUS', 'EOF'
+
+class Token(object):
+    def __init__(self, type, value):
+        self.type = type
+        self.value = value
+
+    def __str__(self):
+        return 'Token({type}, {value})'.format(
+            type=self.type,
+            value=str(self.value))
+
+    def __repr__(self):
+        return self.__str__()
+
+class Interpreter(object):
+    def __init__(self, text):
+        self.text = text
+        self.pos = 0
+        self.current_token = None
+
+    def error(self):
+        raise Exception('Error parsing input')
+
+    def get_next_token(self):
+        pass
