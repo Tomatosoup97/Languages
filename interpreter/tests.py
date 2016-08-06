3cdfbc9c6d56bd4f074d546443b6f5590d10e149
Mateusz Urbańczyk <mateusz.urbanczyk97@gmail.com>
2016-08-06 20:11:49 +0200

    Adjust tests for new syntax

--- interpreter/tests.py
+++ interpreter/tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from interpreter import Interpreter, Lexer
+from interpreter import Parser, Lexer, Interpreter
 
 class LexerTestCase(unittest.TestCase):
     def LexerFactory(self, text):
@@ -32,67 +32,69 @@ class LexerTestCase(unittest.TestCase):
         self.assertEqual(token.value, value)
 
 class CalculationTestCase(unittest.TestCase):
-
     def InterpreterFactory(self, text):
         lexer = Lexer(text)
-        interpreter = Interpreter(lexer)
+        parser = Parser(lexer)
+        interpreter = Interpreter(parser)
         return interpreter
 
     def test_addition(self):
         interpreter = self.InterpreterFactory('20 + 4')
-        self.assertEqual(interpreter.expr(), 24)
+        self.assertEqual(interpreter.interpret(), 24)
+        interpreter = self.InterpreterFactory('2220 + 4')
+        self.assertEqual(interpreter.interpret(), 2224)
 
     def test_substraction(self):
         interpreter = self.InterpreterFactory('16-6')
-        self.assertEqual(interpreter.expr(), 10)
+        self.assertEqual(interpreter.interpret(), 10)
 
     def test_negative_substraction(self):
         interpreter = self.InterpreterFactory('6 - 16')
-        self.assertEqual(interpreter.expr(), -10)
+        self.assertEqual(interpreter.interpret(), -10)
 
     def test_multiplication(self):
         interpreter = self.InterpreterFactory('4 * 3')
-        self.assertEqual(interpreter.expr(), 12)
+        self.assertEqual(interpreter.interpret(), 12)
 
     def test_division(self):
         interpreter = self.InterpreterFactory('12 / 3')
-        self.assertEqual(interpreter.expr(), 4)
+        self.assertEqual(interpreter.interpret(), 4)
 
     def test_precedence_of_operators(self):
         interpreter = self.InterpreterFactory('10 + 4 / 2')
-        self.assertEqual(interpreter.expr(), 12)
+        self.assertEqual(interpreter.interpret(), 12)
 
         interpreter = self.InterpreterFactory('10 + 4 * 2')
-        self.assertEqual(interpreter.expr(), 18)
+        self.assertEqual(interpreter.interpret(), 18)
 
         interpreter = self.InterpreterFactory('4 * 2 + 10')
-        self.assertEqual(interpreter.expr(), 18)
+        self.assertEqual(interpreter.interpret(), 18)
 
         interpreter = self.InterpreterFactory('10 + 4 * 2 - 8')
-        self.assertEqual(interpreter.expr(), 10)
+        self.assertEqual(interpreter.interpret(), 10)
 
     def test_parenthesized_expressions(self):
         interpreter = self.InterpreterFactory('(10 + 4) / 2')
-        self.assertEqual(interpreter.expr(), 7)
+        self.assertEqual(interpreter.interpret(), 7)
 
         interpreter = self.InterpreterFactory('(1 + 4) * 2')
-        self.assertEqual(interpreter.expr(), 10)
+        self.assertEqual(interpreter.interpret(), 10)
         
         interpreter = self.InterpreterFactory('(2 + 1) * (6 - 2)')
-        self.assertEqual(interpreter.expr(), 12)
+        self.assertEqual(interpreter.interpret(), 12)
 
         interpreter = self.InterpreterFactory('1 + 3 * ((4 + 2) / 2)')
-        self.assertEqual(interpreter.expr(), 10)
+        self.assertEqual(interpreter.interpret(), 10)
 
         interpreter = self.InterpreterFactory('7+3 * (10 / (12 / (3+1) - 1))')
-        self.assertEqual(interpreter.expr(), 22)
+        self.assertEqual(interpreter.interpret(), 22)
 
     def test_whitespace_ommition(self):
         interpreter = self.InterpreterFactory('2 +   3')
-        self.assertEqual(interpreter.expr(), 5)
+        self.assertEqual(interpreter.interpret(), 5)
 
         interpreter = self.InterpreterFactory('  7 +  4 ')
-        self.assertEqual(interpreter.expr(), 11)
+        self.assertEqual(interpreter.interpret(), 11)
 
 if __name__ == '__main__':
     unittest.main()
