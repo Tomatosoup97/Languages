1fe3e164fa3dcc88d8908a22676e2acdadfc57da
Mateusz Urbańczyk <murbanczyk@qed.ai>
2016-09-20 18:42:18 +0200

    Edit pascal test case

---
+++ interpreter/tests/case_basic.pas
@@ -0,0 +1,30 @@
+PROGRAM Part10;
+VAR
+   number     : INTEGER;
+   a, b, c    : INTEGER;
+   y          : REAL;
+   str        : STRING;
+   on_fire    : BOOLEAN;
+BEGIN
+   BEGIN 
+      number := 2;
+      a := number;
+      b := 10 * a + 10 * number DIV 4;
+      c := a - (-b);
+      y := 5 / 2
+   END;
+
+   for i := 1 to 10 do
+   BEGIN
+      writeln(i);
+   END;
+
+   on_fire := True;  { Comment }
+   
+   if on_fire then
+      writeln('Mark is on Fire!')
+   else
+      writeln('Mark isnt on Fire');
+
+   writeln(' ');
+END.
